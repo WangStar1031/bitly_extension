78c1f1dc168bee2f368b6172d2af7f86eb35a965
Wang Xing <wangstar1031@hotmail.com>
2019-04-01 23:25:50 +0800

    a bit changed.

--- js/process.js
+++ js/process.js
@@ -8,7 +8,7 @@ jQuery.get(chrome.extension.getURL('temp/input.txt'), function(data) {
     if( arrTemp[i] == "") continue;
     arrOriginals.push(arrTemp[i]);
   }
-  console.log( arrOriginals);
+  // console.log( arrOriginals);
   getNextUrl();
 });
 function postUrl(_longUrl){
@@ -24,13 +24,17 @@ function getShotenUrl(_longUrl){
     }, 1000);
   } else{
     var shortUrl = $("li.shortened_link").eq(0).find(".short-url").text();
+    arrTargets.push(shortUrl);
     console.log(_longUrl, shortUrl);
     nCurrentUrlId++;
     getNextUrl();
   }
 }
+
 function getNextUrl(){
-  if( nCurrentUrlId >= arrOriginals.length)
+  if( nCurrentUrlId >= arrOriginals.length){
+    console.log(shortUrl);
     return;
+  }
   postUrl(arrOriginals[nCurrentUrlId]);
 }
